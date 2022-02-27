8ae00d14f25735c40aca845fd2248ca8f1806202
apardods <tonipardo09@gmail.com>
2022-02-27 23:03:11 +0100

    cutre pero a tiempo

--- docs/api.js
+++ docs/api.js
@@ -1,33 +1,11 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('form').onsubmit = function() {
-
-        // Send a GET request to the URL
-        fetch('http://api.currencylayer.com/live?access_key=6d73f133f2b1f623f84fb7f20f01cd47&currencies=CHF,GBP,EUR,JPY')
-        // Put response into json form
-        .then(response => response.json())
-        .then(data => {
-            // Get currency from user input and convert to upper case
-            const base = "USD";
-            const currency = base.concat(document.querySelector('#currency').value.toUpperCase());
-
-            // Get rate from data
-            const rate = data.quotes[currency];
-
-            // Check if currency is valid:
-            if (rate !== undefined) {
-                // Display exchange on the screen
-                document.querySelector('#result').innerHTML = `1 USD is equal to ${rate.toFixed(3)} ${currency}.`;
-            }
-            else {
-                // Display error on the screen
-                document.querySelector('#result').innerHTML = 'Invalid Currency.';
-            }
-        })
-        // Catch any errors and log them to the console
-        .catch(error => {
-            console.log('Error:', error);
-        });
-        // Prevent default submission
-        return false;
+const loadQuote = async () => {
+    let request = await fetch("https://api.kanye.rest/");
+    if (request.status === 200){
+        if ($("#quote")){
+            $("#boton-principal").html("Otra cita!");
+        }
+        let data = await request.json();
+        let texto = data["quote"];
+        $("#quote").html(texto);
     }
-});
+};
